6190f63796523ce55f58a7d9cd3ad5934771b6b6
bluebitch <wzc345@gmail.com>
2013-03-08 02:14:00 +0800

    SearchBar.

--- WeTongji/WTSearchViewController.m
+++ WeTongji/WTSearchViewController.m
@@ -8,6 +8,7 @@
 
 #import "WTSearchViewController.h"
 #import <WeTongjiSDK/WeTongjiSDK.h>
+#import "WTResourceFactory.h"
 
 @interface WTSearchViewController ()
 
@@ -28,12 +29,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    [self.searchBar.subviews[0] removeFromSuperview];
     
     [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapView:)]];
     
     [self configureNavigationBar];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -43,7 +44,62 @@
 
 #pragma mark - UI methods
 
+- (void)configureSearchBarCancelButton {
+    for (UIView *subView in self.searchBar.subviews) {
+        //Find the button
+        if([subView isKindOfClass:[UIButton class]])
+        {
+            //Change its properties
+            UIButton *cancelButton = (UIButton *)subView;
+            cancelButton.titleLabel.text = @"Changed";
+            
+            [WTResourceFactory configureNormalButton:cancelButton text:NSLocalizedString(@"Cancel", nil)];
+        }
+    }
+}
+
+- (void)configureSearchBarBg:(BOOL)isShowCancelButton {
+    if (isShowCancelButton) {
+        UIImage *selectBgImage = [[UIImage imageNamed:@"WTSearchBarSelectBg"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 30, 0, 30)];
+        [self.searchBar setSearchFieldBackgroundImage:selectBgImage forState:UIControlStateNormal];
+        
+        UIImage *selectSearchIcon = [UIImage imageNamed:@"WTSearchBarSelectSearchIcon"];
+        [self.searchBar setImage:selectSearchIcon forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
+    } else {
+        UIImage *normalBgImage = [[UIImage imageNamed:@"WTSearchBarNormalBg"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 30, 0, 30)];
+        [self.searchBar setSearchFieldBackgroundImage:normalBgImage forState:UIControlStateNormal];
+        
+        UIImage *nornalSearchIcon = [UIImage imageNamed:@"WTSearchBarNormalSearchIcon"];
+        [self.searchBar setImage:nornalSearchIcon forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
+    }
+}
+
 - (void)configureNavigationBar {
+    
+    UISearchBar *searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 1.0f, 270.0f, 44.0f)];
+    searchBar.delegate = self;
+    self.searchBar = searchBar;
+    
+    [searchBar.subviews[0] removeFromSuperview];
+    
+    [self configureSearchBarBg:NO];
+    
+    UIView *searchBarContainerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 270.0f, 44.0F)];
+    searchBarContainerView.backgroundColor = [UIColor clearColor];
+    [searchBarContainerView addSubview:searchBar];
+    
+    UIBarButtonItem *searchBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:searchBarContainerView];
+    
+    self.navigationItem.rightBarButtonItem = searchBarButtonItem;
+    
+}
+
+- (void)showSearchBarCancelButton:(BOOL)show {
+    [self configureSearchBarBg:show];
+    [self.searchBar setShowsCancelButton:show animated:YES];
+    if (show) {
+        // [self configureSearchBarCancelButton];
+    }
 }
 
 #pragma mark - Handle gesture recognizer
@@ -52,7 +108,21 @@
     [self.view endEditing:YES];
 }
 
-#pragma mark - UISearchBarDelegate
+#pragma mark - UISearchBarDelega
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    [self showSearchBarCancelButton:YES];
+    NSLog(@"searchBarTextDidBeginEditing");
+}
+
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    NSLog(@"searchBarTextDidEndEditing");
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    [self showSearchBarCancelButton:NO];
+    [searchBar resignFirstResponder];
+}
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     WTClient *client = [WTClient sharedClient];
@@ -61,8 +131,8 @@
     } failureBlock:^(NSError *error) {
         WTLOGERROR(@"Search user:%@", error.localizedDescription);
     }];
-//    [request search:searchBar.text];
-//    [client enqueueRequest:request];
+    [request search:searchBar.text];
+    [client enqueueRequest:request];
 }
 
 @end
