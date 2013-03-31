f9a48093c4a3ae6228e7f93be1fa5bacc86beddf
bluebitch <wzc345@gmail.com>
2013-03-31 12:50:37 +0800

    添加PlaceholderImage。

---
+++ WeTongji/WTScrollView.m
@@ -0,0 +1,38 @@
+//
+//  WTScrollView.m
+//  WeTongji
+//
+//  Created by 王 紫川 on 13-3-31.
+//  Copyright (c) 2013年 Tongji Apple Club. All rights reserved.
+//
+
+#import "WTScrollView.h"
+
+@interface WTScrollView ()
+
+@property (nonatomic, strong) UIImageView *topPlaceholderImageView;
+
+@end
+
+@implementation WTScrollView
+
+- (void)didMoveToSuperview {
+    if (!self.topPlaceholderImageView.superview) {
+        [self addSubview:self.topPlaceholderImageView];
+        [self sendSubviewToBack:self.topPlaceholderImageView];
+    }
+}
+
+#pragma mark - Properties
+
+- (UIImageView *)topPlaceholderImageView {
+    if (!_topPlaceholderImageView) {
+        UIImage *placeholderImage = [UIImage imageNamed:@"WTBluePlaceholderImage.jpg"];
+        _topPlaceholderImageView = [[UIImageView alloc] initWithImage:placeholderImage];
+        [_topPlaceholderImageView setAutoresizingMask:UIViewAutoresizingNone];
+        [_topPlaceholderImageView resetOriginY:-_topPlaceholderImageView.frame.size.height];
+    }
+    return _topPlaceholderImageView;
+}
+
+@end
