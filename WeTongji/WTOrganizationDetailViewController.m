7a60c9356d9b2fdc7a5e5df091916a21af9a4a12
Ziqi Wu <Ziqi.Wu@bmw.com>
2013-05-16 23:31:15 +0800

    Ziqi:创建Banner View

--- WeTongji/WTOrganizationDetailViewController.m
+++ WeTongji/WTOrganizationDetailViewController.m
@@ -9,13 +9,16 @@
 #import "WTOrganizationDetailViewController.h"
 #import "Organization+Addition.h"
 #import "UIImageView+AsyncLoading.h"
-#import "WTScrollView.h"
+#import "WTBannerView.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface WTOrganizationDetailViewController ()
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UIView *organizationHeaderView;
-@property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
-@property (weak, nonatomic) IBOutlet UILabel *organizationNameLabel;
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet UIView *organizationHeaderView;
+@property (nonatomic, weak) IBOutlet UIImageView *logoImageView;
+@property (nonatomic, weak) IBOutlet UILabel *organizationNameLabel;
+
+@property (nonatomic, strong) WTBannerContainerView *bannerView;
 @property (nonatomic, strong) Organization *org;
 
 @end
@@ -59,10 +62,22 @@
 
 - (void)configureUI {
     [self configureOrganizationHeaderView];
+    [self configureBannerView];
+    [self configureScrollView];
+}
+
+- (void)configureScrollView {
+    self.scrollView.alwaysBounceVertical = YES;
+    self.scrollView.scrollsToTop = YES;
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.contentSize.width, self.scrollView.bounds.size.height + 1);
 }
 
 - (void)configureOrganizationHeaderView {
     self.organizationHeaderView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"WTGrayPanel"]];
+    self.organizationHeaderView.layer.shadowColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1.0f].CGColor;
+    self.organizationHeaderView.layer.shadowOffset = CGSizeMake(0, 1.0f);
+    self.organizationHeaderView.layer.shadowOpacity = 0.25f;
+    self.organizationHeaderView.layer.shadowRadius = 0;
     
     [self configureOrganizationLogoAndName];
 }
@@ -73,6 +88,28 @@
     self.organizationNameLabel.text = self.org.name;
     [self.organizationNameLabel sizeToFit];
     [self.organizationNameLabel resetCenterX:self.view.bounds.size.width / 2];
+}
+
+- (void)configureBannerView {
+    self.bannerView = [WTBannerContainerView createBannerContainerView];
+    [self.bannerView resetOrigin:CGPointMake(0, self.organizationHeaderView.frame.origin.y + self.organizationHeaderView.frame.size.height)];
+    [self.scrollView insertSubview:self.bannerView atIndex:0];
+    [self configureTestBanner];
+}
+
+- (void)configureTestBanner {
+    NSArray *orgNameArray = @[@"WeTongji Dev Team", @"Tongji Apple Club", @"Apple Inc."];
+    NSArray *titleArray = @[@"WeTongji 3.0 Coming Soon", @"Enroll 2012", @"WWDC 2011"];
+    for(int i = 0; i < orgNameArray.count; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"WTTestBanner%d", i + 1];
+        UIImage *image = [UIImage imageNamed:imageName];
+        
+        [self.bannerView addItemViewWithImage:image
+                         titleText:titleArray[i]
+                  organizationName:orgNameArray[i]
+                             style:WTBannerItemViewStyleBlue
+                           atIndex:i];
+    }
 }
 
 #pragma mark - Actions
