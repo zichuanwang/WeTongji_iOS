1b8264ff955bb91ee6acd8a246a6fbb7c88bdc53
bluebitch <wzc345@gmail.com>
2013-05-27 23:43:54 +0800

    济人排序修正。

--- WeTongji/Core Data/Star.h
+++ WeTongji/Core Data/Star.h
@@ -2,7 +2,7 @@
 //  Star.h
 //  WeTongji
 //
-//  Created by 王 紫川 on 13-5-7.
+//  Created by 王 紫川 on 13-5-27.
 //  Copyright (c) 2013年 Tongji Apple Club. All rights reserved.
 //
 
@@ -13,15 +13,16 @@
 
 @interface Star : Object
 
-@property (nonatomic, retain) NSString * motto;
-@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSString * avatar;
 @property (nonatomic, retain) NSNumber * canLike;
-@property (nonatomic, retain) NSNumber * likeCount;
+@property (nonatomic, retain) NSString * content;
 @property (nonatomic, retain) id imageArray;
 @property (nonatomic, retain) NSString * jobTitle;
-@property (nonatomic, retain) NSString * studentNumber;
-@property (nonatomic, retain) NSString * content;
+@property (nonatomic, retain) NSNumber * likeCount;
+@property (nonatomic, retain) NSString * motto;
+@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSString * starNumber;
+@property (nonatomic, retain) NSString * studentNumber;
+@property (nonatomic, retain) NSDate * createdAt;
 
 @end
--- WeTongji/Core Data/Star.m
+++ WeTongji/Core Data/Star.m
@@ -2,7 +2,7 @@
 //  Star.m
 //  WeTongji
 //
-//  Created by 王 紫川 on 13-5-7.
+//  Created by 王 紫川 on 13-5-27.
 //  Copyright (c) 2013年 Tongji Apple Club. All rights reserved.
 //
 
@@ -11,15 +11,16 @@
 
 @implementation Star
 
-@dynamic motto;
-@dynamic name;
 @dynamic avatar;
 @dynamic canLike;
-@dynamic likeCount;
+@dynamic content;
 @dynamic imageArray;
 @dynamic jobTitle;
-@dynamic studentNumber;
-@dynamic content;
+@dynamic likeCount;
+@dynamic motto;
+@dynamic name;
 @dynamic starNumber;
+@dynamic studentNumber;
+@dynamic createdAt;
 
 @end
